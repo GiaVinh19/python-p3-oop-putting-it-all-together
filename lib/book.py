27cd2f15629b2a109faabd3fb4c540eb3ceb351a
GiaVinhNguyen <vinh.g.nguyen19@gmail.com>
2024-02-22 14:45:14 -0500

    lab's status: defined

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        assert isinstance(page_count, int), "page_count must be an integer"
+        self.title = title
+        self.page_count = page_count
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+book1 = Book("Made in Abyss", 100)
+book1.turn_page()
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        assert isinstance(size, int), "size must be an integer"
+        self.brand = brand
+        self.size = size
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+shoe1 = Shoe("Puma", 10)
+shoe1.cobble()
+print(shoe1.condition)
